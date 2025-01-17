46b4f1c3fde6fefb66c605aa297d37e40c4cbdbb
EduardoSa23 <eduardo.sa2398@gmail.com>
2025-01-17 16:53:43 -0300

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react"
+
+import Perfil from "./components/Perfil"
+import ReposList from "./components/reposList"
+
+function App() {
+  const [formularioEstaVisivel, setFormularioEstaVisivel] = useState(true)
+  const [nomeUsuario, setNomeUsuario] = useState('') 
+
+  return (
+    <>
+    <input type="text" 
+    onBlur={(e) => setNomeUsuario(e.target.value)} 
+    onKeyDown={(e) => {
+    if (e.key === "Enter") {
+      e.target.blur(); // Simula o evento onBlur ao pressionar Enter
+    }
+  }}  />
+
+    {nomeUsuario.length > 4 && (
+      <>
+        <Perfil nomeUsuario={nomeUsuario} />
+        <ReposList nomeUsuario={nomeUsuario} />
+      </>
+    )}
+    </>
+  )
+}
+
+export default App
